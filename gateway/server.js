6eff2e8aed0790bb799bab50ad20eb5e7d2a2fdf
Ralf <isewat@gmail.com>
2018-12-12 16:24:57 +0100

    initialized project

---
+++ gateway/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    msg: 'Hi from Gateway'
+  });
+});
+
+app.get('/data', async (req, res) => {
+  const data = await axios.get('http://helper:3001/data');
+  res.json(data.data);
+});
+
+app.listen(3000, () => {
+  console.log('Gateway listening on port 3000');
+});
+
+module.exports.app = app;
---
+++ helper/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    msg: 'Hi from helper'
+  });
+});
+
+app.get('/data', (req, res) => {
+  res.json({
+    data: [{ key: 1, value: 5 }, { key: 2, value: 7 }, { key: 3, value: 9 }]
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Helper listening on port 3001');
+});
